02050529bc7ca8750b57deb7cd83f18c1abae306
halimawebd <inthesumon1010@gmail.com>
2025-06-04 20:30:57 +0600

    add the verifytoken

--- index.js
+++ index.js
@@ -3,13 +3,62 @@ const cors = require('cors')
 const app = express();
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
+const logger = (req, res, next) => {
+    console.log('inside the logger middleware');
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+const verifyFirebaseToken = (req, res, next) => {
+    const authHeader = req.headers?.authorization;
+    const token = authHeader.split('')[1];
+    if (token) { return } {
+        return res.status(401).send({message: 'unauthorized access'})
+    }
+    console.log('fb token', token)
+}
+
+// const verifyToken = (req, res, next) => {
+//     const token = req?.cookies.token;
+//     console.log('cookie in the middleware', req.cookies)
+//     if (!token) {
+//         return res.status(401).send({ message: 'unauthorized access' })
+//     }
+//     // verify token
+//     jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) => {
+//         if (err) {
+//             return res.status(401).send({message: 'unathorized access'})
+//         }
+//         req.decoded = decoded;
+//         next();
+//     })
+
+// }
 // careerDB
 // poENzKYeCbVtrmfB
 
@@ -35,13 +84,20 @@ async function run() {
         const applicationsCollection = client.db('careerCode').collection('application');
 
 
-        // iwt token related api
+        // jwt token related api
         app.post('/jwt', async (req, res) => {
-            const { email } = req.body;
-            const user = { email }
-            const token = jwt.sign(user, 'secret', { expiresIn: '1h' });
-            res.send({token})
+            const userInfo = req.body;
+
+            const token = jwt.sign(userInfo, process.env.JWT_ACCESS_SECRET, { expiresIn: '2h' });
+
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: false
+            })
+
+            res.send({ success: true })
         })
+
         // jobs api
         app.get('/jobs', async (req, res) => {
             const email = req.query.email;
@@ -55,13 +111,6 @@ async function run() {
             res.send(result);
         });
 
-        // could be done
-        // app.get('/jobsByEmailAddress', async (req, res) => {
-        //     const email = req.query.email;
-        //     const query = { hr_email: email }
-        //     const result = await jobsCollection.find(query).toArray();
-        //     res.send(result)
-        // })
 
         app.get('/applications/job/:job_id', async (req, res) => {
             const job_id = req.params.job_id;
@@ -77,6 +126,20 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/jobs/applications', verifyToken, async (req, res) => {
+            const email = req.query.email;
+            const query = { hr_email: email };
+            const jobs = await jobsCollection.find(query).toArray();
+
+            // should use aggregate to have optimum data featching
+
+            for (const job of jobs) {
+                const applicationQuery = { jobId: job._id.toString() }
+                const application_count = await applicationsCollection.countDocuments(applicationQuery)
+                job.application_count = application_count;
+            }
+        })
+
         app.post('/jobs', async (req, res) => {
             const newJob = req.body;
             console.log(newJob);
@@ -85,8 +148,10 @@ async function run() {
         })
 
         // job application related apis
-        app.get('/applications', async (req, res) => {
+        app.get('/applications', logger, verifyFirebaseToken, async (req, res) => {
             const email = req.query.email;
+            // console.log('inside application api', req.cookies);
+
 
             const query = {
                 application: email
